7bf2e191f7f491f3b4a9389d345ae4df8e8a03a2
René Wilhelm <rene.wilhelm@gmail.com>
2014-09-05 01:59:51 +0200

    Basic routes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,34 @@
-console.log('This would be the main JS file.');
+angular.module('app', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/asdf', {
+	  templateUrl: 'asdf.html',
+	  controller: 'asdfCtrl'
+	})
+  .when('/qwer', {
+	  templateUrl: 'qwer.html',
+	  controller: 'qwerCtrl'
+	})
+  .when('/wsad', {
+	  templateUrl: 'wsad.html',
+	  controller: 'wsadCtrl'
+	});
+  $locationProvider.html5Mode(false);
+})
+
+.controller('appCtrl', ['$scope', function($scope) {
+	$scope.title = 'asdf';
+}])
+
+.controller('asdfCtrl', ['$scope', function($scope) {
+	$scope.title = 'asdf';
+}])
+
+.controller('qwerCtrl', ['$scope', function($scope) {
+	$scope.title = 'qwer';
+}])
+
+.controller('wsadCtrl', ['$scope', function($scope) {
+	$scope.title = 'wsad';
+}]);
